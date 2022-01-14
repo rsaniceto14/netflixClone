781bb1a53e529806d03297d6726caedfb38b2090
rsaniceto14 <raphaelaniceto14@gmail.com>
2022-01-14 14:36:18 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Tmdb from './Tmdb';
+import { useEffect } from 'react';
+import MovieRow from './components/MovieRow';
+import './App.css';
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+
+useEffect(()=>{
+const loadAll = async () => {
+  // Pegando list
+
+  let list = await Tmdb.getHomeList();
+  setMovieList(list);
+
+  }
+
+   loadAll();
+
+
+  },  []);
+
+  return (
+
+    <div className='page'>
+     
+           <section className='lists'>
+        {movieList.map((item, key)=> (
+            <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+
+
+
+    </div>
+
+  );
+  
+
+}
